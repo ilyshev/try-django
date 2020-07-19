3accce2e9ff46789b4be71a689791dae9e5eda34
ilyshev <dilyshev@mac.com>
2020-07-19 18:11:41 -0400

    creating urls

--- src/pages/views.py
+++ src/pages/views.py
@@ -5,10 +5,11 @@ from django.shortcuts import render
 def home_view(request, *args, **kwargs):
 	print(*args, **kwargs)
 	print(request.user)
-	return HttpResponse("<h1>Hello world</h1>")
+	return render(request, 'home.html', {})
+	# return HttpResponse("<h1>Hello world</h1>")
 
 def contact_view(request, *args, **kwargs):
-	return HttpResponse("<h1> Contact Page </h1>")
+	return render(request, 'contact.html', {})
 
 def about_view(request, *args, **kwargs):
-	return HttpResponse("<h1> About Page </h1>")
+	return render(request, 'about.html', {})
