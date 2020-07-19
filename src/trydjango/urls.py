55b3c28ca05a8596f7c5499ab669bbf8a6a4e6bc
ilyshev <dilyshev@mac.com>
2020-07-19 15:47:45 -0400

    initial commit

--- src/trydjango/urls.py
+++ src/trydjango/urls.py
@@ -1,7 +1,7 @@
 """trydjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,19 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
-
-from pages.views import home_view, contact_view, about_view
-
+from django.urls import path
 
 urlpatterns = [
-    path('blog/', include('blog.urls')),
-    path('courses/', include('courses.urls')),
-    path('products/', include('products.urls')),
-    path('', home_view, name='home'),
-    path('about/<int:id>/', about_view, name='product-detail'),
-    path('contact/', contact_view),
     path('admin/', admin.site.urls),
 ]
-
-
