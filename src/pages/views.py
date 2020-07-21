529a0d18bd94707dd238d06fcb6c1e4b2d920393
ilyshev <dilyshev@mac.com>
2020-07-20 21:21:30 -0400

    working with templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -12,4 +12,11 @@ def contact_view(request, *args, **kwargs):
 	return render(request, 'contact.html', {})
 
 def about_view(request, *args, **kwargs):
-	return render(request, 'about.html', {})
+	my_context = {
+		'title': 'This is about us',
+		'my_number': 123,
+		'this_is_true': True,
+		'my_list': [123, 4564, 312, 993, 'abc'],
+		'my_html': '<h1>Hello World</h1>',
+	}
+	return render(request, 'about.html', my_context)
