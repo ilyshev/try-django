d8deb35a3ff72e4d295b84413c643746cb3ef6f4
ilyshev <dilyshev@mac.com>
2020-07-27 12:15:04 -0400

    finishing

--- src/courses/urls.py
+++ src/courses/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import (
+	CourseCreateView,
+	CourseDeleteView,
 	CourseListView,
 	CourseView,
+	CourseUpdateView,
 	# my_fbv
 )
 
@@ -10,9 +13,9 @@ app_name = 'courses'
 urlpatterns = [
 	path('', CourseListView.as_view(), name='courses-list'),
 	# path('', my_fbv, name='courses-list')
- #    path('create/', CourseCreateView.as_view(), name='course-create'),
-    # path('<int:id>/update', ArticleUpdateView.as_view(), name='article-update'),
+    path('create/', CourseCreateView.as_view(), name='course-create'),
+    path('<int:id>/update', CourseUpdateView.as_view(), name='courses-update'),
     path('<int:id>/', CourseView.as_view(), name='courses-detail'), #pk!! not id if there isn't def get_object() in .views
-    # path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
+    path('<int:id>/delete/', CourseDeleteView.as_view(), name='courses-delete'),
     
 ]
--- src/courses/views.py
+++ src/courses/views.py
@@ -1,9 +1,84 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import Course
+from .forms import CourseModelForm
 # Create your views here.
 
 #BASE VIEW CLASS = View
+class CourseObjectMixin(object):
+	model = Course
+
+	def get_object(self):
+		id = self.kwargs.get('id')
+		obj = None
+		if id is not None:
+			obj = get_object_or_404(self.model, id=id)	
+		return obj
+
+class CourseDeleteView(CourseObjectMixin, View):
+	template_name = 'courses/course_delete.html'
+	def get(self, request, id=None, *args, **kwargs):
+		#GET method
+		context = {}
+		obj = self.get_object()
+		if obj is not None:
+			form = CourseModelForm(instance=obj)
+			context['object'] = obj
+		return render(request, self.template_name, context)
+
+	def post(self, request, id=None, *args, **kwargs):
+		#POST method
+		context = {}
+		obj = self.get_object()
+		if obj is not None:
+			obj.delete()
+			context['object'] = None
+			return redirect('/courses/')
+		return render(request, self.template_name, context)
+
+class CourseUpdateView(CourseObjectMixin, View):
+	template_name = 'courses/course_update.html'
+	def get(self, request, id=None, *args, **kwargs):
+		#GET method
+		context = {}
+		obj = self.get_object()
+		if obj is not None:
+			form = CourseModelForm(instance=obj)
+			context['object'] = obj
+			context['form'] = form
+		return render(request, self.template_name, context)
+
+	def post(self, request, id=None, *args, **kwargs):
+		#POST method
+		form = CourseModelForm(request.POST)
+		context = {}
+		obj = self.get_object()
+		if obj is not None:
+			form = CourseModelForm(request.POST, instance=obj)
+			if form.is_valid():
+				form.save()
+			context['object'] = obj
+			context['form'] = form
+		return render(request, self.template_name, context)
+
+
+class CourseCreateView(View):
+	template_name = 'courses/course_create.html'
+	def get(self, request, *args, **kwargs):
+		#GET method
+		form = CourseModelForm()
+		context = {'form': form}
+		return render(request, self.template_name, context)
+
+	def post(self, request, *args, **kwargs):
+		#POST method
+		form = CourseModelForm(request.POST)
+		if form.is_valid():
+			form.save()
+			form = CourseModelForm()
+		context = {'form': form}
+		return render(request, self.template_name, context)
+
 
 class CourseListView(View):
 	template_name = 'courses/courses_list.html'
@@ -16,14 +91,12 @@ class CourseListView(View):
 		context = {'object_list': self.get_queryset()}
 		return render(request, self.template_name, context)
 
-class CourseView(View):
+
+class CourseView(CourseObjectMixin, View):
 	template_name = 'courses/course_detail.html'
 	def get(self, request, id=None, *args, **kwargs):
 		#GET method
-		context = {}
-		if id is not None:
-			obj = get_object_or_404(Course, id=id)
-			context['object'] = obj
+		context = {'object': self.get_object()}
 		return render(request, self.template_name, context)
 	
 	# def post(request,*args, **kwargs):
